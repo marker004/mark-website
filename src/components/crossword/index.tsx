34c169f81f6225b43bc566eeeadfa06e94541680
mreckard <mark.reckard@gmail.com>
2023-11-14 15:52:48 -0500

    export types

--- src/components/crossword/index.tsx
+++ src/components/crossword/index.tsx
@@ -1,1 +1,12 @@
 export { Crossword } from "./crossword";
+
+export type {
+  Matrix15x15,
+  CellContents,
+  PuzzleDirection,
+  HorizontalDirection,
+  VerticalDirection,
+  CursorDirection,
+  CellCoordinates,
+  Clues,
+} from "./types";
